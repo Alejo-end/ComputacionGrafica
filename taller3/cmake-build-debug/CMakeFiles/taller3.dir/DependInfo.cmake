0557b1d41416c5a602ca3b896368e2bdbc0b3833
Alejandro <aleonskater@gmail.com>
2020-05-01 18:33:48 -0500

    actualizacion

--- taller3/cmake-build-debug/CMakeFiles/taller3.dir/DependInfo.cmake
+++ taller3/cmake-build-debug/CMakeFiles/taller3.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/Alejandro/Documents/Semestre_1_2020/ComputacionGrafica/taller3/main.cpp" "C:/Users/Alejandro/Documents/Semestre_1_2020/ComputacionGrafica/taller3/cmake-build-debug/CMakeFiles/taller3.dir/main.cpp.obj"
+  "C:/Users/Alejandro/Documents/Semestre_1_2020/ComputacionGrafica/ComputacionGrafica/taller3/main.cpp" "C:/Users/Alejandro/Documents/Semestre_1_2020/ComputacionGrafica/ComputacionGrafica/taller3/cmake-build-debug/CMakeFiles/taller3.dir/main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
