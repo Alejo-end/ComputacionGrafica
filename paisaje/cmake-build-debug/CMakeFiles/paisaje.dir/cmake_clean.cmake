e1d0d0ed6ebdca1356b77dc6992752ab0e7d7a09
Alejandro <aleonskater@gmail.com>
2020-05-17 12:44:18 -0500

    agregando proyecto paisaje

---
+++ paisaje/cmake-build-debug/CMakeFiles/paisaje.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/paisaje.dir/main.cpp.obj"
+  "libpaisaje.dll.a"
+  "paisaje.exe"
+  "paisaje.exe.manifest"
+  "paisaje.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/paisaje.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
